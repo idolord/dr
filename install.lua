d5b3b40e56e4172d0c666a648414ea61a88d06f2
quentin <wantiezquentin@gmail.com>
2018-10-18 03:04:51 +0200

    main

--- install.lua
+++ install.lua
@@ -1,15 +1,16 @@
-local libURL = "https://raw.githubusercontent.com/idolord/dr/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/idolord/dr/master/dr.lua"
-local lib, startup
-local libFile, startupFile
+local reactorURL = "https://raw.githubusercontent.com/idolord/dr/master/reactorCommands.lua"
+local startupURL = "https://raw.githubusercontent.com/idolord/dr/master/startup.lua"
+local reactorIfnoUrl = "https://raw.githubusercontent.com/idolord/dr/master/reactorinfo.lua"
+local rebootScript = "https://raw.githubusercontent.com/idolord/dr/master/rebootScript.lua"
 
-fs.makeDir("lib")
+local lib, startup
+local reactor, startupFile, reactorinfo, rebootSRC
 
 lib = http.get(libURL)
-libFile = lib.readAll()
+reactor = lib.readAll()
 
-local file1 = fs.open("lib/f", "w")
-file1.write(libFile)
+local file1 = fs.open("reactorCommands", "w")
+file1.write(reactor)
 file1.close()
 
 startup = http.get(startupURL)
@@ -18,4 +19,20 @@ startupFile = startup.readAll()
 
 local file2 = fs.open("startup", "w")
 file2.write(startupFile)
+file2.close()
+
+startup = http.get(reactorIfnoUrl)
+reactorinfo = startup.readAll()
+
+
+local file3 = fs.open("reactorinfo", "w")
+file2.write(reactorinfo)
+file2.close()
+
+startup = http.get(reactorIfnoUrl)
+rebootSRC = startup.readAll()
+
+
+local file4 = fs.open("rebootScript", "w")
+file2.write(rebootSRC)
 file2.close()
---
+++ startup.lua
@@ -0,0 +1,7 @@
+UI = shell.openTab("reactorCommands")
+
+shell.openTab("reactorinfo")
+
+sleep(1)
+
+shell.switchTab(UI)
